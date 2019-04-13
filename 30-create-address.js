c91d0c5360757ebfda5a4a6703a663ee0ec5c399
John Shearing <johnshearing@gmail.com>
2019-04-13 14:39:36 -0400

    Update 30-create-address.js

--- 30-create-address.js
+++ 30-create-address.js
@@ -6,6 +6,11 @@ const iotaLibrary = require('@iota/core')
 
 const iota = iotaLibrary.composeAPI({})
 
+// Your seed should look something like the following:
+// FATTCNIQXGTTCSKIUVPRQXNRAGSHDGTGGGTOHQPMDBXHSDCNHPYQRCNI9AXDLIVCCA9KATTHSOSZZOIDV
+// Don't use the seed above. Use your own seed.  
+// Paste your seed between the quotes below.  
+
 const seed =
   'Paste-Your-Seed-Between-These-Quotes'
 
