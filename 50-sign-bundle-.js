23085b7f21c459ef7ae680c4afa0f5390cf5e8b9
John Shearing <johnshearing@gmail.com>
2019-04-14 02:46:14 -0400

    Update 50-sign-bundle-.js

--- 50-sign-bundle-.js
+++ 50-sign-bundle-.js
@@ -7,15 +7,15 @@ const { createPrepareTransfers } = require('@iota/core');
 const { asciiToTrytes } = require('@iota/converter');
 
 
-let seed = 'FATTCNIQXGTTCSKIUVPRQXNRAGSHDGTGGGTOHQPMDBXHSDCNHPYQRCNI9AXDLIVCCA9KATTHSOSZZOIDV';
-let fromAddress = 'DVZRJOKM9KQKRLLIQPQAWASCQGBHYJURXGOBAJPZNHHQAYCXTZFQZJTIBX9OQHOHFDNLQWFYGWRFALSBXBQPACGMUA';
-let fromAddressIndex = 0;
-let fromAddressBalance = 17500000;
-let toAddress = 'AMECMIEELJKYZPQLBVSMJLSVPQFDSDZHTIWGORVJD9OUDGYINMOABRINKLW9HFVSFAKUQSWTJGCGSILS9MGNBSOAIA';
-let securityLevel = 2;
-let transferAmount = 4000000;
-let remainderAddress = 'AYKJNRHMHA9CHSLREPDJBRMYLPOBJRGYPFYYNDSVBJYWEIEVCEQPRBQSEHXSFKLCBQJADSZSYJLUKBCGWKDHOCYROD';
-let message = 'test';
+let seed = 'Paste-Your-Seed-Between-These-Quotes';
+let fromAddress = 'Paste-Your-Spending-Address-Between-These-Quotes';
+let fromAddressIndex = 0; //Substitute the zero with the index of your spending address.
+let fromAddressBalance = 0; //Substitute the zero with the amount of IOTAs in your spending address.
+let toAddress = 'Paste-The-Recipient-Address-Between-These-Quotes';
+let securityLevel = 2; //Substitute the 2 with the security level you chose when you made your spending address.
+let transferAmount = 0; //Substitute the zero with the amount of IOTAs you wish to spend.
+let remainderAddress = 'Paste-Your-Remainder-Address-Between-These-Quotes';
+let message = ''; // You can paste as message between the quotes if you want.
   
 
 // Construct a transfers object
