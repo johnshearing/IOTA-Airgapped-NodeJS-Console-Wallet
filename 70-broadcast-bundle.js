8026fbb147ab57904b2fd0c20e0afeb0bc20222a
John Shearing <johnshearing@gmail.com>
2019-04-18 02:22:05 -0400

    Update 70-broadcast-bundle.js

--- 70-broadcast-bundle.js
+++ 70-broadcast-bundle.js
@@ -9,7 +9,7 @@ const iota = iotaLibrary.composeAPI({
   //Lookup healthy nodes at https://iota.dance/
   //provider: 'https://nodes.devnet.thetangle.org:443'
   //provider: 'https://nodes.thetangle.org:443'
-  provider: 'Paste-The-URL-Of-A-Healthy-Node-Here'
+  provider: 'Paste-The-URL-Of-A-Healthy-Node-Between-These-Quotes'
 })
 
 
