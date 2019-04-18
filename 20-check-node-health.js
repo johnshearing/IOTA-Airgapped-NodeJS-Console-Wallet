efe2f01a5370f8fcbb89a56cc087c024eef84136
John Shearing <johnshearing@gmail.com>
2019-04-18 02:11:24 -0400

    Update 20-check-node-health.js

--- 20-check-node-health.js
+++ 20-check-node-health.js
@@ -6,7 +6,8 @@ const iotaLibrary = require('@iota/core')
 
 const iota = iotaLibrary.composeAPI({
   //Lookup healthy nodes at https://iota.dance/
-  provider: 'https://nodes.thetangle.org:443'
+  //provider: 'https://nodes.thetangle.org:443'
+  provider: 'Paste-The-URL-Of-A-Healthy-Node-Here'
 })
 
 iota
