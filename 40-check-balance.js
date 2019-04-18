72657fed31b22ef06c16804f5958a28d4c47ca34
John Shearing <johnshearing@gmail.com>
2019-04-18 02:11:46 -0400

    Update 40-check-balance.js

--- 40-check-balance.js
+++ 40-check-balance.js
@@ -9,7 +9,7 @@ const iota = iotaLibrary.composeAPI({
   //Lookup healthy nodes at https://iota.dance/
   //provider: 'https://nodes.devnet.thetangle.org:443'
   //provider: 'https://nodes.thetangle.org:443'
-  provider: 'Paste-The-URL-Of-A-Health-Node-Here'
+  provider: 'Paste-The-URL-Of-A-Healthy-Node-Here'
 })
 
 // Your address should look something like the following:
